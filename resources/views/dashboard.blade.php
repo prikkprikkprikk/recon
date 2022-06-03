5455c04f0e17ebbc77780743b57e07ecf2f4ee3b
Jørn Støylen <jorn@digitaltwist.no>
2022-06-03 09:58:49 +0200

    Add instructions to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,16 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <div class="p-6 bg-white border-b border-gray-200 prose">
+                    <h1>
+                        Filformat
+                    </h1>
+                    <p>
+                        Last ned excel-fil fra bankens arkivsøk.
+                    </p>
+                    <p>
+                        NB! Bruk altså <em>ikke</em> nedlasting fra «Inn og ut av konto»-siden.
+                    </p>
                 </div>
             </div>
         </div>
