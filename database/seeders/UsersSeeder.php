6e40ea8ab72385b244339a09fe6b4814662f1cbf
Jørn Støylen <jorn@digitaltwist.no>
2022-11-06 09:05:59 +0100

    Fix admin user env variables

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -15,9 +15,9 @@ class UsersSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => env('ADMIN_NAME'),
-            'email' => env('ADMIN_EMAIL'),
-            'password' => env('ADMIN_PASSWORD'),
+            'name' => env('ADMIN_USER_NAME'),
+            'email' => env('ADMIN_USER_EMAIL'),
+            'password' => env('ADMIN_USER_PASSWORD'),
         ]);
     }
 }
